5052ffa01a4bfbe9e3fbfa01c33fceba9504a901
farhan <farhanfadhilah87@gmail.com>
2023-10-01 18:50:18 +0700

    migrate to rds aws

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "root:@tcp(127.0.0.1:3306)/d_gita_db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "dgita:dgita123@tcp(dgita-db.cbuoaypgqh0v.us-east-1.rds.amazonaws.com:3306)/dgita?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
